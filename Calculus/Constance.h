f6a97c863a045263f53f56d4bf3f380e1c03ca4a
tracedeng <1401733434@qq.com>
2016-01-22 11:37:57 +0800

    网络请求IP

--- Calculus/Constance.h
+++ Calculus/Constance.h
@@ -10,22 +10,22 @@
 #define Constance_h
 
 #ifdef DEBUG
-#define ACCOUNTURL @"http://localhost:8000/account"
-#define CONSUMERURL @"http://localhost:8000/consumer"
-#define MERCHANTURL @"http://localhost:8000/merchant"
-#define CREDITURL @"http://localhost:8000/credit"
-#define FLOWTURL @"http://localhost:8000/flow"
+#define ACCOUNTURL @"http://192.168.1.157:8000/account"
+#define CONSUMERURL @"http://192.168.1.157:8000/consumer"
+#define MERCHANTURL @"http://192.168.1.157:8000/merchant"
+#define CREDITURL @"http://192.168.1.157:8000/credit"
+#define FLOWTURL @"http://192.168.1.157:8000/flow"
 #define QINIUURL @"http://7xor5x.com1.z0.glb.clouddn.com"
-#define BUSINESSURL @"http://localhost:8000/business"
-#define FLOWURL @"http://localhost:8000/flow"
-#define DISCOUNTURL @"http://localhost:8000/activity"
-#define ACTIVITYURL @"http://localhost:8000/activity"
-#define VOUCHERURL @"http://localhost:8000/voucher"
+#define BUSINESSURL @"http://192.168.1.157:8000/business"
+#define FLOWURL @"http://192.168.1.157:8000/flow"
+#define DISCOUNTURL @"http://192.168.1.157:8000/activity"
+#define ACTIVITYURL @"http://192.168.1.157:8000/activity"
+#define VOUCHERURL @"http://192.168.1.157:8000/voucher"
 #else
-#define CONSUMERURL @"http://localhost/consumer"
-#define MERCHANTURL @"http://localhost/merchant"
-#define CREDITURL @"http://localhost/credit"
-#define FLOWTURL @"http://localhost/flow"
+#define CONSUMERURL @"http://www.weijifen.me:8000/consumer"
+#define MERCHANTURL @"http://www.weijifen.me:8000/merchant"
+#define CREDITURL @"http://www.weijifen.me:8000/credit"
+#define FLOWTURL @"http://www.weijifen.me:8000/flow"
 #endif
 
 #endif /* Constance_h */
