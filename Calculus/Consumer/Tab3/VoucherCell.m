e1f5901e0ca9086047f02c9e08f1f5c22e275304
tracedeng <1401733434@qq.com>
2016-02-21 19:26:54 +0800

    voucher

--- Calculus/Consumer/Tab3/VoucherCell.m
+++ Calculus/Consumer/Tab3/VoucherCell.m
@@ -7,13 +7,17 @@
 //
 
 #import "VoucherCell.h"
+#import "Constance.h"
+#import "UIImageView+WebCache.h"
 
 @interface VoucherCell ()
 @property (weak, nonatomic) IBOutlet UILabel *activiyTitle
 ;
 @property (weak, nonatomic) IBOutlet UILabel *expireLabel;
 @property (weak, nonatomic) IBOutlet UILabel *activityMerchant;
+@property (weak, nonatomic) IBOutlet UIImageView *merchantLogo;
 
+@property (weak, nonatomic) IBOutlet UIImageView *backgroundImageView;
 @end
 
 @implementation VoucherCell
@@ -29,11 +33,17 @@
 }
 
 - (void)setVoucherInfo:(NSDictionary *)voucherInfo {
+//    self.backgroundImageView.clipsToBounds = YES;
+//    self.backgroundImageView.layer.cornerRadius = 4.0f;
     if (voucherInfo) {
         _voucherInfo = voucherInfo;
         self.activiyTitle.text = [voucherInfo objectForKey:@"ti"];
         self.activityMerchant.text = [voucherInfo objectForKey:@"na"];
         self.expireLabel.text = [[voucherInfo objectForKey:@"et"] substringToIndex:10];
+
+        self.merchantLogo.layer.cornerRadius = 4.0f;
+        NSString *path = [NSString stringWithFormat:@"%@/%@?imageView2/1/w/200/h/200", QINIUURL, [voucherInfo objectForKey:@"logo"]];
+        [self.merchantLogo sd_setImageWithURL:[NSURL URLWithString:path]];
     }
 }
 @end
