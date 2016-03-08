78850dd791e6802cf426307c30b7737a8134d2be
ben <guoliben@163.com>
2016-03-08 20:18:28 +0800

    更新我的优惠券详情背景

--- Calculus/Consumer/Tab3/Voucher/VoucherDetailsVC.m
+++ Calculus/Consumer/Tab3/Voucher/VoucherDetailsVC.m
@@ -25,23 +25,23 @@
     // Do any additional setup after loading the view.
     
     //    圆角
-    self.voucherBackgroundVIew.clipsToBounds = YES;
-    self.voucherBackgroundVIew.layer.cornerRadius = 4.0f;
-    self.voucherBackgroundVIew.layer.borderWidth = 1.0f;
-    self.voucherBackgroundVIew.layer.borderColor = [[UIColor colorWithHex:0x555555] CGColor];
+//    self.voucherBackgroundVIew.clipsToBounds = YES;
+//    self.voucherBackgroundVIew.layer.cornerRadius = 4.0f;
+//    self.voucherBackgroundVIew.layer.borderWidth = 1.0f;
+//    self.voucherBackgroundVIew.layer.borderColor = [[UIColor colorWithHex:0x555555] CGColor];
 
 //    self.avatarImageView.layer.cornerRadius = self.avatarImageView.frame.size.height / 2.0;
     
     
-    self.voucherTitleView.layer.borderWidth = 1.0f;
-    self.voucherTitleView.layer.borderColor = [[UIColor colorWithHex:0xDDDDDD] CGColor];
+//    self.voucherTitleView.layer.borderWidth = 1.0f;
+//    self.voucherTitleView.layer.borderColor = [[UIColor colorWithHex:0xDDDDDD] CGColor];
 
     self.voucherNumberView.layer.borderWidth = 1.0f;
     self.voucherNumberView.layer.borderColor = [[UIColor colorWithHex:0xDDDDDD] CGColor];
     
-    self.voucherQrCodeView.layer.borderWidth = 1.0f;
-    self.voucherQrCodeView.layer.borderColor = [[UIColor colorWithHex:0xDDDDDD] CGColor];
-
+//    self.voucherQrCodeView.layer.borderWidth = 1.0f;
+//    self.voucherQrCodeView.layer.borderColor = [[UIColor colorWithHex:0xDDDDDD] CGColor];
+//
     
     
 }
