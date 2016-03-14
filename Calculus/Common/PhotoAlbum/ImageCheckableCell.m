6aa1338053a66b2fc32f0612899c157d67b85d1a
ben <guoliben@163.com>
2016-03-14 17:20:27 +0800

    头像选择按钮透明度

--- Calculus/Common/PhotoAlbum/ImageCheckableCell.m
+++ Calculus/Common/PhotoAlbum/ImageCheckableCell.m
@@ -19,7 +19,7 @@
 - (void)setAssetImage:(UIImage *)assetImage {
     [super setAssetImage:assetImage];
     [self.checkButton setBackgroundImage:[UIImage imageNamed:@"photo-uncheck"] forState:UIControlStateNormal];
-    self.checkButton.alpha = 0.5;
+    self.checkButton.alpha = 0.8;
 }
 
 - (void)switchCheckState {
@@ -31,7 +31,7 @@
     }else{
         //未选中状态
         [self.checkButton setBackgroundImage:[UIImage imageNamed:@"photo-uncheck"] forState:UIControlStateNormal];
-        self.checkButton.alpha = 0.5;
+        self.checkButton.alpha = 0.8;
     }
 }
 
