72d9ed7478d041d0b4c6fca24134566c149f4108
ben <guoliben@163.com>
2016-03-26 20:29:30 +0800

    增加客户端积分列表 认证"V"图标

--- Calculus/Consumer/Tab1/Award/MyAwardCell.m
+++ Calculus/Consumer/Tab1/Award/MyAwardCell.m
@@ -15,6 +15,7 @@
 @property (weak, nonatomic) IBOutlet UIImageView *merchant_logo_image;
 @property (weak, nonatomic) IBOutlet UILabel *merchant_name_label;
 @property (weak, nonatomic) IBOutlet UILabel *total_award_label;
+@property (nonatomic, retain) IBOutlet UIImageView *defaultimage;
 
 @end
 
@@ -36,6 +37,7 @@
 //    self.merchant_logo_image.image = [UIImage imageNamed:@"icon-mcd"];
 //    self.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
     
+ 
     //圆角
     self.merchant_logo_image.clipsToBounds = YES;
     self.merchant_logo_image.layer.cornerRadius = 4.0f;
@@ -43,5 +45,23 @@
 
     NSString *path = [NSString stringWithFormat:@"%@/%@?imageView2/1/w/300/h/300", QINIUURL, [awardInfo objectForKey:@"l"]];
     [self.merchant_logo_image sd_setImageWithURL:[NSURL URLWithString:path] placeholderImage:[UIImage imageNamed:@"logo-merchant-default"]];
+
+    
+    self.defaultimage = [[UIImageView alloc] init];
+    self.defaultimage.image=[UIImage imageNamed:@"icon-v"];
+    //    self.defaultimage.frame=CGRectMake( deviceWidth *1/8, (deviceHeight - deviceWidth *3/4) / 4,  deviceWidth *3/4, deviceWidth *3/4 );
+    self.defaultimage.frame=CGRectMake( self.merchant_logo_image.frame.origin.x + 35,  self.merchant_logo_image.frame.origin.y + 35, 15, 15 );
+    self.defaultimage.layer.cornerRadius = self.defaultimage.frame.size.width / 2.0f;
+    self.defaultimage.clipsToBounds = YES;
+    self.defaultimage.backgroundColor = [UIColor whiteColor];
+    
+    if([[awardInfo objectForKey:@"v"] isEqualToString:@"yes"]){
+        
+        [self.merchant_logo_image addSubview:self.defaultimage];
+
+    }
+    
+    
+
 }
 @end
